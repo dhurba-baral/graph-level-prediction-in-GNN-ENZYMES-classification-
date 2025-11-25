e84fb3a93ae7e712c3880fa68e9739c0954c8ea6
Dhurba Baral <dhurbabaralam@gmail.com>
2025-11-24 19:29:51 -0500

    train different models

--- train_model.py
+++ train_model.py
@@ -1,39 +1,57 @@
 import torch
 import os
-import torch.optim as optim
+import numpy as np
+from torch import optim
 from models import GCN_1Layer, GCN_2Layer, GAT_Model
 from train_functions import train_model
-from data_loaders import BATCH_SIZE
 from data_loaders import train_loader, val_loader, num_features, num_classes
-
-#reproducibility
-torch.manual_seed(42)
+from plot_functions import plot_loss, plot_accuracies
 
 # Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"\nUsing device: {device}")
 
-#make directory to save models, plots
-os.makedirs('models', exist_ok=True)
-os.makedirs('plots', exist_ok=True)
+# Set random seed for reproducibility
+torch.manual_seed(42)
+np.random.seed(42)
 
 #hyperparameters
-NUM_EPOCHS = 100
-LEARNING_RATE = 0.01
 HIDDEN_DIM = 64
+LEARNING_RATE = 0.01
+NUM_EPOCHS = 100
 DROPOUT = 0.5
 
 #loss function
 criterion = torch.nn.CrossEntropyLoss()
 
-#path to save model
-model1_path = f'models/GCN1_ENZYMES.pth'
-
 #train GCN1 model
-model1 = GCN_1Layer(num_features, HIDDEN_DIM, num_classes, DROPOUT)
+model1 = GCN_1Layer(num_features=num_features, hidden_channels=HIDDEN_DIM, num_classes=num_classes, dropout=DROPOUT)
 optimizer = optim.Adam(model1.parameters(), lr=LEARNING_RATE, weight_decay=5e-4)
-trained_model, train_losses, val_losses, train_accuracies, val_accuracies, train_f1_scores, val_f1_scores, train_auc_scores, val_auc_scores = train_model(
+
+train_losses, val_losses, train_accuracies, val_accuracies, train_f1_scores, val_f1_scores, train_auc_scores, val_auc_scores = train_model(
             model1, train_loader, val_loader, optimizer, criterion,
             device, NUM_EPOCHS, 'GCN1', num_classes
         )
-torch.save(trained_model.state_dict(), model_path)
+plot_loss(train_losses=train_losses, val_losses=val_losses, model_name='GCN1')
+plot_accuracies(train_accuracies=train_accuracies, val_accuracies=val_accuracies, model_name='GCN1')
+
+#train GCN2 model
+model2 = GCN_2Layer(num_features=num_features, hidden_channels=HIDDEN_DIM, num_classes=num_classes, dropout=DROPOUT)
+optimizer = optim.Adam(model2.parameters(), lr=LEARNING_RATE, weight_decay=5e-4)
+
+train_losses, val_losses, train_accuracies, val_accuracies, train_f1_scores, val_f1_scores, train_auc_scores, val_auc_scores = train_model(
+            model2, train_loader, val_loader, optimizer, criterion,
+            device, NUM_EPOCHS, 'GCN2', num_classes
+        )
+plot_loss(train_losses=train_losses, val_losses=val_losses, model_name='GCN2')
+plot_accuracies(train_accuracies=train_accuracies, val_accuracies=val_accuracies, model_name='GCN2')
+
+#train GAT model
+model3 = GAT_Model(num_features=num_features, hidden_channels=HIDDEN_DIM, num_classes=num_classes, heads=4, dropout=DROPOUT)
+optimizer = optim.Adam(model3.parameters(), lr=LEARNING_RATE, weight_decay=5e-4)
+
+train_losses, val_losses, train_accuracies, val_accuracies, train_f1_scores, val_f1_scores, train_auc_scores, val_auc_scores = train_model(
+            model3, train_loader, val_loader, optimizer, criterion,
+            device, NUM_EPOCHS, 'GAT', num_classes
+        )
+plot_loss(train_losses=train_losses, val_losses=val_losses, model_name='GAT')
+plot_accuracies(train_accuracies=train_accuracies, val_accuracies=val_accuracies, model_name='GAT')
